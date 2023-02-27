23a9e3e38ddb3be8eef520dd85d21610fa54cbc9
oldbai <65586402+oldbai555@users.noreply.github.com>
2023-02-27 11:16:59 +0800

    Update sys_cmd.go
    补充查找application.yaml的路径

--- lbserver/impl/sys_cmd.go
+++ lbserver/impl/sys_cmd.go
@@ -81,6 +81,7 @@ func initViper() (*viper.Viper, error) {
 	viper.SetConfigName("application")          // name of config file (without extension)
 	viper.SetConfigType("yaml")                 // REQUIRED if the config file does not have the extension in the name
 	viper.AddConfigPath("/etc/work/")           // path to look for the config file in
+	viper.AddConfigPath("./") // optionally look for config in the working directory
 	viper.AddConfigPath("./lbserver/cmd/")      // optionally look for config in the working directory
 	viper.AddConfigPath("./lbserver/resource/") // optionally look for config in the working directory
 	err := viper.ReadInConfig()                 // Find and read the config file
