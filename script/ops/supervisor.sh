d5e75899832a7f1e72bb8bf4aada2a3c161b2e7a
zhangjianjun <1005777562@qq.com>
2024-09-03 20:05:30 +0800

    feat: 脚本优化

--- script/ops/supervisor.sh
+++ script/ops/supervisor.sh
@@ -3,17 +3,21 @@
 # 设置环境变量
 # source .env
 
-# 取代码目录
 sh_dir=$(cd "$(dirname "$0")" && pwd)
-echo "sh dir:"$sh_dir
+echo "脚本目录 shDir:"$sh_dir
+
+# 项目目录
 pro_dir=$(dirname ${sh_dir})
-projectRootDir=$(dirname ${pro_dir})
-echo "projectRootDir:"${projectRootDir}
+echo "项目目录 proDir:"${pro_dir}
+
+# 项目根目录
+proRootDir=$(dirname ${pro_dir})
+echo "项目根目录 proRootDir:"${proRootDir}
 
 # 定义全局变量
 supervisorDir="/home/work/service"
 supervisorLogDir="/home/work/supervisor/logs"
-packOutputDir="${projectRootDir}/package"
+packOutputDir="${proRootDir}/package"
 
 # 检查并创建日志目录 打包输出目录
 mkdir -p "$supervisorDir"
@@ -25,12 +29,11 @@ chmod +x -R "$supervisorLogDir"
 outputSupervisorConf() {
   local programName=$1
   local outputDir=$2
-  local appName=$3
 
   cat >"$outputDir/$programName.conf" <<EOF
 [program:$programName]
 directory=$supervisorDir/$programName
-command=$supervisorDir/$programName/$appName
+command=$supervisorDir/$programName/$programName
 autostart=true
 autorestart=true
 startsecs=10
@@ -44,48 +47,27 @@ stdout_logfile_backups=20
 EOF
 }
 
-# 函数：打包并部署网关服务
-localPackGate() {
-  local programName=$1
-  local outputDir="$packOutputDir/$programName"
-  mkdir -p "$outputDir"
-
-  outputSupervisorConf "$programName" "$outputDir" "$programName"
-  goBuild "$outputDir" "$projectRootDir/service/$programName"
-  cp "$projectRootDir/service/$programName/application.yaml" "$outputDir"
-  tar -cvf "$outputDir.tar" -C "$outputDir" .
-
-  deploySupervisorService "$programName"
-}
-
-# 函数：打包并部署服务器服务
-localPackServer() {
-  local appName=$1
-  local outputDir="$packOutputDir/$appName"
-  mkdir -p "$outputDir"
-
-  outputSupervisorConf "$appName" "$outputDir" "$appName"
-  goBuild "$outputDir" "$projectRootDir/service/${appName}server/cmd" $appName
-  cp "$projectRootDir/service/${appName}server/cmd/application.yaml" "$outputDir"
-  tar -cvf "$outputDir.tar" -C "$outputDir" .
-
-  deploySupervisorService "$appName"
-}
-
 # 函数：部署 Supervisor 服务
 deploySupervisorService() {
   local appName=$1
 
   cd "$supervisorDir" || exit
+
   rm -rf "$appName"
+
   mkdir -p "$appName"
+
   tar -xvf "$packOutputDir/$appName.tar" -C "$supervisorDir/$appName"
+
   chmod +x -R "$supervisorDir/$appName"
+
   cp "$supervisorDir/$appName/$appName.conf" /etc/supervisor/conf.d
 
   # 更新并重启 Supervisor 服务
   supervisorctl update
+
   supervisorctl restart "$appName"
+
   # 检查服务状态
   supervisorctl status "$appName"
 }
@@ -103,11 +85,33 @@ goBuild() {
   unset GOARCH
 }
 
+# 函数：打包并部署服务
+localPackSrv() {
+  # 服务名称
+  local appName=$1
+
+  # 打包路径
+  local proSrvPath=$2
+
+  # 输出目录
+  local outputDir="$packOutputDir/$appName"
+
+  mkdir -p ${outputDir}
+
+  outputSupervisorConf ${appName} ${outputDir}
+
+  goBuild ${outputDir} ${proSrvPath}
+
+  cp "${proSrvPath}/application.yaml" ${outputDir}
+
+  tar -cvf "${appName}.tar" -C "$outputDir" --remove-files
+}
+
 # 使用说明
 usage() {
   echo "Usage: sh supervisor.sh [OPTION]"
-  echo "  lpg | localPackGate: 打包并部署网关服务"
-  echo "  lps | localPackServer: 打包并部署服务器服务"
+  echo "  lpg: 打包网关"
+  echo "  lpcmd: 打包服务"
   # ... 可以添加更多选项
 }
 
@@ -121,11 +125,13 @@ fi
 case "$1" in
 lpg)
   shift
-  localPackGate "$1"
+  localPackSrv "$1" ${proRootDir}"/service/gateway"
+  deploySupervisorService "$1"
   ;;
-lps)
+lpcmd)
   shift
-  localPackServer "$1"
+  localPackSrv "$1" ${proRootDir}"/singlesrv/cmd"
+  deploySupervisorService "$1"
   ;;
 *)
   usage
